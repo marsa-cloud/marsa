0a0318443c8bd2d89730607c04b53ec2d4aa6c1a
Mohammad Gomaa <midomaxgomaa@gmail.com>
2026-05-26 18:03:40 +0300

    feat(api): annotate status endpoint for openapi

--- apps/api/src/modules/status/use-cases/get-api-info/get-api-info.controller.ts
+++ apps/api/src/modules/status/use-cases/get-api-info/get-api-info.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 
-import type { GetApiInfoResponse } from '#src/modules/status/use-cases/get-api-info/get-api-info.response.js'
+import { GetApiInfoResponse } from '#src/modules/status/use-cases/get-api-info/get-api-info.response.js'
 import { GetApiInfoService } from '#src/modules/status/use-cases/get-api-info/get-api-info.service.js'
 
+@ApiTags('status')
 @Controller({ path: 'status', version: '1' })
 export class GetApiInfoController {
   constructor(private readonly service: GetApiInfoService) {}
 
   @Get()
+  @ApiOperation({ operationId: 'getApiInfo' })
+  @ApiOkResponse({ type: GetApiInfoResponse })
   handle(): GetApiInfoResponse {
     return this.service.execute()
   }
